d59a26c9bcfc169cd875ae3281d13b78f11b3aa9
Mohamad SALMAN <hamoudi_lb@hotmail.com>
2019-04-29 00:38:54 +0300

    Update before day3a

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -15,5 +15,7 @@ namespace DatingApp.Data
         public DbSet<Values> Value { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
